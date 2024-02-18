7dec720e6237698dae6653898a4faf9e2f0fcdd4
MD-2005 <jee.emd2005@gmail.com>
2024-02-18 22:34:04 +0530

    you gae

---
+++ 2023/EE/wrong assignment/codes/A_1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+n_1=np.arange(0, 17)
+n_2=np.array([3, 9, 15])
+y1=np.loadtxt("series.dat", delimiter=" ", max_rows=1)
+y2=y1[n_2]
+plt.stem(n_1, y1, markerfmt='.', linefmt='-', basefmt='r', label=r'$y(n)$') 
+plt.stem(n_2, y2, markerfmt='o', linefmt='-') 
+
+plt.xlabel('n')
+plt.ylabel('x(n)')
+plt.grid(True)
+plt.legend()
+plt.title('Stem plot of x(n)')
+plt.savefig('../figs/A_1.png')
